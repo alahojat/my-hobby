d786d1e76e4cfc7733f0ead8a2d0b232bbe918bf
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-03-28 13:20:47 +0100

    :sparkles: feat(main.ts) print homePage and render its html.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,35 @@
 import './style.css'
 
+let homePageContainer: HTMLElement = document.getElementById('homePageContainer')!;
 
+
+function appendHomePageHtml() {
+let homePage = document.createElement('div');
+homePage.classList.add('homePageContainer');
+
+let header = document.createElement('header');
+let nav = document.createElement('nav');
+
+let pageTitle = document.createElement('h1');
+pageTitle.innerText = "Ala Hojat";
+
+let artBtn = document.createElement('button');
+artBtn.innerText = "mixed media";
+
+let photoBtn = document.createElement('button');
+photoBtn.innerText = "photography";
+
+nav.append(pageTitle, artBtn, photoBtn);
+header.append(nav);
+homePage.append(header);
+homePageContainer.append(homePage);
+}
+
+
+function printHomePage() {
+    appendHomePageHtml();
+    console.log("hej");
+
+}
+
+printHomePage();
